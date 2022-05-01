f5808a51bc7ceaadb848d5cff6436b34f8b618d3
HamzaBenyazid <benyzidhamza969@gmail.com>
2022-05-01 20:00:59 +0000

    commented Role and Team class

--- src/main/java/com/winchesters/devopsify/model/Team.java
+++ src/main/java/com/winchesters/devopsify/model/Team.java
@@ -1,21 +1,21 @@
-package com.winchesters.devopsify.model;
-
-
-import lombok.*;
-
-import javax.persistence.*;
-
-
-@NoArgsConstructor
-@AllArgsConstructor
-@Setter
-@Getter
-@Entity
-@Table
-public class Team {
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long teamId;
-    private String name;
-
-}
+//package com.winchesters.devopsify.model;
+//
+//
+//import lombok.*;
+//
+//import javax.persistence.*;
+//
+//
+//@NoArgsConstructor
+//@AllArgsConstructor
+//@Setter
+//@Getter
+//@Entity
+//@Table
+//public class Team {
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.AUTO)
+//    private Long teamId;
+//    private String name;
+//
+//}
