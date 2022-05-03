8f2d9c3737eb52a17b8ec30d2faf75b2b11dfbb5
obaydah bouifadene <obaydah.bouifadene@um5r.ac.ma>
2022-05-03 13:55:58 +0000

    fixed not working validation

--- src/main/java/com/winchesters/devopsify/controller/GeneralControllerAdvice.java
+++ src/main/java/com/winchesters/devopsify/controller/GeneralControllerAdvice.java
@@ -7,22 +7,32 @@ import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.converter.HttpMessageConversionException;
+import org.springframework.web.bind.MissingRequestHeaderException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import javax.validation.ValidationException;
 
 @Order()
 @RestControllerAdvice
 public class GeneralControllerAdvice {
     private static final Logger LOG = LoggerFactory.getLogger(GeneralControllerAdvice.class);
+
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     @ExceptionHandler(Exception.class)
     public void handleException(Exception exception) {
         LOG.error("Technical exception", exception);
     }
+
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    @ExceptionHandler({ ConstraintViolationException.class, HttpMessageConversionException.class })
+    @ExceptionHandler({
+            ConstraintViolationException.class,
+            HttpMessageConversionException.class,
+            ValidationException.class,
+            MissingRequestHeaderException.class})
     public void handleBadRequest(Exception exception) {
         LOG.debug("Bad request {}", exception.toString());
     }
+
 }
