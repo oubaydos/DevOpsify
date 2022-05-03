5deeba2da2febafddc05fa4539abd9663456b47e
obaydah bouifadene <obaydah.bouifadene@um5r.ac.ma>
2022-05-03 01:00:38 +0000

    added github controller advice

--- src/main/java/com/winchesters/devopsify/controller/github/GithubController.java
+++ src/main/java/com/winchesters/devopsify/controller/github/GithubController.java
@@ -8,22 +8,24 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 
+@Validated
 @RestController
 @RequestMapping(path = "api/v1/github")
 @RequiredArgsConstructor
 public class GithubController {
     private static final Logger LOG = LoggerFactory.getLogger(GithubController.class);
     private final GithubServiceImpl githubService;
-    private final GithubRepositoryImpl githubRepository;
+//    private final GithubRepositoryImpl githubRepository;
     @PostMapping("/connect")
     @ResponseStatus( HttpStatus.CREATED )
     private void connect(@RequestHeader String personalAccessToken) throws IOException {
         LOG.debug("connect request: {}", personalAccessToken);
-        githubService.connectToGithub(personalAccessToken);
-        LOG.debug("connect response: {}", HttpStatus.CREATED);
+        GitHub gitHub = githubService.connectToGithub(personalAccessToken);
+        LOG.debug("connect response: {}", gitHub.toString());
     }
 }
