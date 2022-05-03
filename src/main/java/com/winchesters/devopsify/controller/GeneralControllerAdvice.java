f0cc3efab7189a192ecee4cef2d51a60e8d6a649
obaydah bouifadene <obaydah.bouifadene@um5r.ac.ma>
2022-05-03 01:00:32 +0000

    added general controller advice

---
+++ src/main/java/com/winchesters/devopsify/controller/GeneralControllerAdvice.java
@@ -0,0 +1,28 @@
+package com.winchesters.devopsify.controller;
+
+import org.hibernate.exception.ConstraintViolationException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.converter.HttpMessageConversionException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Order()
+@RestControllerAdvice
+public class GeneralControllerAdvice {
+    private static final Logger LOG = LoggerFactory.getLogger(GeneralControllerAdvice.class);
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ExceptionHandler(Exception.class)
+    public void handleException(Exception exception) {
+        LOG.error("Technical exception", exception);
+    }
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler({ ConstraintViolationException.class, HttpMessageConversionException.class })
+    public void handleBadRequest(Exception exception) {
+        LOG.debug("Bad request {}", exception.toString());
+    }
+}
