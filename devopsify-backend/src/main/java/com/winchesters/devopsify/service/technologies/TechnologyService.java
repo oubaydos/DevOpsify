d49c41b7d5dff81dffe45fe790744fd73a183529
obaydah bouifadene <obaydah.bouifadene@um5r.ac.ma>
2022-06-03 19:44:00 +0100

    added homeDirectory

--- devopsify-backend/src/main/java/com/winchesters/devopsify/service/technologies/TechnologyService.java
+++ devopsify-backend/src/main/java/com/winchesters/devopsify/service/technologies/TechnologyService.java
@@ -1,12 +1,15 @@
 package com.winchesters.devopsify.service.technologies;
 
+import org.springframework.beans.factory.annotation.Value;
+
+import javax.validation.Valid;
 import java.io.File;
 import java.io.IOException;
 
 public interface TechnologyService {
 
     String scriptDirectory = System.getProperty("user.dir").concat("/scripts/");
-
+    String homeDirectory = javax.swing.filechooser.FileSystemView.getFileSystemView().getHomeDirectory().toString();
     boolean installed();
 
     default boolean installed(String technology){
