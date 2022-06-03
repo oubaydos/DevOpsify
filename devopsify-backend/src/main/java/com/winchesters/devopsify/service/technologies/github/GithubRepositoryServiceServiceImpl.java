76f4060eabda130c48b93e4d4d1c6e44a7f090c4
obaydah bouifadene <obaydah.bouifadene@um5r.ac.ma>
2022-06-03 21:06:10 +0100

    refactor

--- devopsify-backend/src/main/java/com/winchesters/devopsify/service/technologies/github/GithubRepositoryServiceServiceImpl.java
+++ devopsify-backend/src/main/java/com/winchesters/devopsify/service/technologies/github/GithubRepositoryServiceServiceImpl.java
@@ -14,7 +14,7 @@ import java.util.Optional;
 @Service
 @RequiredArgsConstructor
 @Transactional
-public class GithubRepositoryImpl implements GithubRepository {
+public class GithubRepositoryServiceServiceImpl implements GithubRepositoryService {
     private final GithubServiceImpl githubService;
 
     @Override
