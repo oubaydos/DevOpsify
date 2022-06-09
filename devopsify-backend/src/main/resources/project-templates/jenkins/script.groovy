c037f8b15e7753688c0541911a35434f991f58e4
obaydah bouifadene <obaydah.bouifadene@um5r.ac.ma>
2022-06-09 20:02:31 +0100

    push docker image Jenkinsfile stage

--- devopsify-backend/src/main/resources/project-templates/jenkins/script.groovy
+++ devopsify-backend/src/main/resources/project-templates/jenkins/script.groovy
@@ -33,4 +33,12 @@ def buildImage(){
     echo "${IMAGE_TAG}"
     sh "docker build -t oubaydos/temp:${IMAGE_TAG} ."
 }
+def pushImage(){
+    echo "pushing the docker image"
+    // TODO pushed to nexus
+    withCredentials([usernamePassword(credentialsId: 'dockerhub', passwordVariable: 'PASSWORD', usernameVariable: 'USERNAME')]){
+        sh "echo $PASSWORD | docker login -u $USERNAME --password-stdin"
+        sh "docker push oubaydos/temp:$IMAGE_TAG"
+    }
+}
 return this
