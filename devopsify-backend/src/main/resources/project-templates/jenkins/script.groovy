b1bbfd0151bbe30f591cc14d48e4646c977a1d78
obaydah bouifadene <obaydah.bouifadene@um5r.ac.ma>
2022-06-10 20:37:14 +0100

    version commit image in jenkinsfile

--- devopsify-backend/src/main/resources/project-templates/jenkins/script.groovy
+++ devopsify-backend/src/main/resources/project-templates/jenkins/script.groovy
@@ -51,5 +51,14 @@ def deployImage() {
         }
     }
 }
-
+def commitVersion(){
+    withCredentials([usernamePassword(credentialsId: 'githubWithToken', passwordVariable: 'PASSWORD', usernameVariable: 'USERNAME')]){
+        sh "git config user.name 'jenkins'"
+        sh "git config user.email 'jenkins@oubaydos.com'"
+        sh "git remote set-url origin https://${PASSWORD}@github.com/HamzaBenyazid/account-sharing-app"
+        sh "git add . "
+        sh "git commit -m 'update version'"
+        sh "git push origin HEAD:${BRANCH_NAME}"
+    }
+}
 return this
