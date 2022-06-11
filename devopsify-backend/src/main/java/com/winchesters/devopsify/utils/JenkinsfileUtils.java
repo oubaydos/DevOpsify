4dff602356a00bfb60f52918968e55a7e92bdeca
obaydah bouifadene <obaydah.bouifadene@um5r.ac.ma>
2022-06-11 22:51:57 +0100

    added jenkinsFileDto to JenkinsFile mapper

--- devopsify-backend/src/main/java/com/winchesters/devopsify/utils/JenkinsfileUtils.java
+++ devopsify-backend/src/main/java/com/winchesters/devopsify/utils/JenkinsfileUtils.java
@@ -1,10 +1,28 @@
 package com.winchesters.devopsify.utils;
 
+import com.winchesters.devopsify.dto.request.JenkinsFileDto;
+import com.winchesters.devopsify.service.technologies.jenkins.JenkinsFile;
+
 import java.io.File;
 import java.io.IOException;
 
 public class JenkinsfileUtils extends FilesUtils {
     public JenkinsfileUtils(File file) throws IOException {
         super(file, "//");
+    }
+    public static JenkinsFile jenkinsFileDtoToJenkinsFile(JenkinsFileDto dto, Boolean defaultJenkinsFile) {
+        if (defaultJenkinsFile) {
+            return JenkinsFile.builder().setWithDeployment(false).build();
+        }
+        return JenkinsFile.builder()
+                .setImageName(dto.getImageName())
+                .setDockerhubUsername(dto.getDockerhubUsername())
+                .setEc2Username(dto.getEc2Username())
+                .setEc2Ip(dto.getEc2Ip())
+                .setEc2ContainerPort(dto.getEc2ContainerPort())
+                .setEc2DeploymentPort(dto.getEc2DeploymentPort())
+                .setWithDeployment(dto.getWithDeployment())
+                .setGithubRepositoryUrl(dto.getGithubRepositoryUrl())
+                .build();
     }
 }
